39b2f90f28081b81ea2c28d8ef6cc46a8aebcb65
Tarun Tailor <tarun@crimecheck.in>
2023-10-01 21:59:40 +0530

    .

---
+++ Blog Tutorial/Joke Tutorial/app/root.tsx
@@ -0,0 +1,20 @@
+import { LiveReload, Outlet } from "@remix-run/react";
+
+export default function App() {
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1"
+        />
+        <title>Remix: So great, it's funny!</title>
+      </head>
+      <body>
+        <Outlet />
+        <LiveReload />
+      </body>
+    </html>
+  );
+}
